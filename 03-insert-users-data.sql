b29624df7722a986faf4068d24ba3e9957d297b0
s4remi <s4remi@gmail.com>
2024-02-03 23:36:19 -0800

    clean up

--- 03-insert-users-data.sql
+++ 03-insert-users-data.sql
@@ -1,3 +1,6 @@
-INSERT into users (yearly_salary,full_name,current_status)VALUES (19000,'Max Schwarz','self-employed');
+INSERT into users (yearly_salary,full_name,current_status)VALUES (25000,'Julie Barnes','employed');
+--delete from users where full_name='Julie Barnes';
+SELECT * from users
+
 
 
