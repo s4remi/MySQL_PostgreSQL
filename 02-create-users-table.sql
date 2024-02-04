d6e54c5cc18f713ec70bd6fb00a66b0730aa7dd1
s4remi <s4remi@gmail.com>
2024-02-03 22:57:30 -0800

    add users table for mysql and postgresql

--- 02-create-users-table.sql
+++ 02-create-users-table.sql
@@ -1,1 +1,8 @@
-CREATE TABLE users;
+--in Postgresql as there is no ENUM we can create the type first like below
+CREATE type employment_status as ENUM('employed','self-employed','unemployed');
+CREATE TABLE users(
+    full_name VARCHAR(200),
+    yearly_salary INT,
+    current_status employment_status
+    --current_status ENUM('employed','self-employed','unemployed')-- MySQL syntax ,
+);
