02ce77468678c6e1aa31eeb9c505f368b8a12b23
s4remi <s4remi@gmail.com>
2024-02-22 01:14:25 -0800

    innerjoin

--- 16-innerjoin-relation.sql
+++ 16-innerjoin-relation.sql
@@ -1,8 +1,10 @@
 -- inner join
 
-SELECT first_name,last_name,street,house_number,c.name as city-name
+SELECT first_name,last_name,street,house_number,c.name as city_name
 from users as usr
 INNER JOIN addresses as addr ON usr.address_id=addr.id
-INNER JOIN cities as c ON addr.city_id= c.id;
+INNER JOIN cities as c ON addr.city_id= c.id
+where c.name='Berlin' or c.name='London'
+ORDER BY usr.id;
 
 
